b27465373438f9532556d1ec62a04b1b29ade412
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-07-22 10:21:21 -0400

    added bkgd image to header, edited padding on main ocntainer

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-let movieCard = document.getElementById("movieCard");
-let innerDiv = document.querySelector(".inner");
-let watchTrailerMsg = document.getElementById("watchTrailer");
-let ytLinkName;
+let title;
+let type;
+let year;
 
 $("#search-Btn").on("click", function (event) {
   event.preventDefault();
-  // let title = $("#movie_title").val().trim();
-  apiCall();
-
+  title = $("#title").val().trim().toLowerCase();
+  type = $("#type").val().trim().toLowerCase();
+  year = $("#year").val().trim().toLowerCase();
+  // console.log(title, type, year);
+  getData(title, type, year);
 });
 
-function apiCall() {
+function getData(title, type, year) {
   axios
-    .get(`http://www.omdbapi.com/?apikey=b00e7121&t=thor&type=movie&page=10`)
+    .get(
+      `http://www.omdbapi.com/?apikey=b00e7121&t=${title}&type=${type}&y=${year}`
+    )
     .then((response) => {
       console.log(response.data);
-      // let movieData = response.data;
-      // displayMovieData(movieData);
+      document.getElementById("myForm").reset();
+      displayData(response.data);
     })
     .catch((error) => console.error(error));
 }
 
-function displayMovieData(data) {
-  const { Title, Actors, Genre, Year, Rated, Director, Runtime, Plot, Poster } =
+function displayData(data) {
+  const { Title, Year, Genre, Actors, Rated, Plot, Director, Runtime, Poster } =
     data;
 
-  ytLinkName = Title.split(" ").join("+").toLowerCase();
-  innerDiv.style.opacity = -1;
-  movieCard.style.display = "block";
-
-  $("#movieCard").append(
+  $("#movieCard-container").append(
     `
-    <div id="movieInfoCard" >
-      <h3 class="movieTitle data" onmouseenter="showTrailerMsg()">${Title}</h3>
-      <img src="${Poster}" id="poster" onmouseenter="showTrailerMsg()" alt=${Title} - Movie Poster />
-        <div class="movieData">
-          <p class="movieGenre data">Genre: ${Genre}</p>
-          <p class="movieActors data">Actors: ${Actors}</p>
-          <p class="movieYear data">Year: ${Year}</p>
-          <p class="movieRated data">Rated: ${Rated}</p>
-          <p class="movieDirector data">Director: ${Director}</p>
-          <p class="moviePlot data">Plot: ${Plot}</p>
-          <p class="movieRuntime data">Runtime: ${Runtime}</p>
-        </div>
-        <div class="backBtnContainer">
-          <button id="backBtn" onclick="location.reload()">Back</button>
+    <div id="movieCard" >
+      <img src="${Poster}" alt=${Title}/>
+        <div class="movie-card-body">
+          <h2>${Title}</h2>
+          <p>Genre: ${Genre}</p>
+          <p>Actors: ${Actors}</p>
+          <p>Year: ${Year}</p>
+          <p>Rated: ${Rated}</p>
+          <p>Plot: ${Plot}</p>
+          <p>Director: ${Director}</p>
+          <p>Runtime: ${Runtime}</p>
         </div>
     </div>
     `

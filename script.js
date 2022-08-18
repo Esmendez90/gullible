269e8037f5ee1df6a6ee15caca3f44460bf9c8b6
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-08-18 12:14:38 -0400

    added function that will re-render the movie data when clicked on the storage section

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let title;
 let type;
 let year;
-let acc = []
+let movieCardEl = document.getElementById("imgBtn");
 
 function getData(title, year) {
+  $("#movieCard").remove();
+  document.querySelector("#movieCard-container").scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "start",
+  });
   axios
     .get(
       `http://www.omdbapi.com/?apikey=b00e7121&t=${title}&y=${year}&plot=full`
@@ -12,6 +18,7 @@ function getData(title, year) {
       if (response.data.Response === "False") {
         alert("Error! Please, try again. Check your spelling.");
       } else {
+        // console.log(response.data);
         let storage = JSON.parse(localStorage.getItem("saved-movies-series"));
         storage.unshift(response.data);
         localStorage.setItem("saved-movies-series", JSON.stringify(storage));
@@ -47,27 +54,14 @@ function renderStorage() {
   let storage = JSON.parse(localStorage.getItem("saved-movies-series"));
   for (let i = 0; i < storage.length; i++) {
     $("#storage-container").append(`
-    <div id="movieCard">
+    <div id="movieCard" onclick="myf(event)">
   
        
-        <img src="${storage[i].Poster}" alt=${storage[i].Title}/>
+      <img src="${storage[i].Poster}" alt="${storage[i].Title}" id="${storage[i].Title}" />
         
-       <button class="accordion">Expand</button>
-        <div class="movie-card-body panel" >
-           <h1>${storage[i].Title}</h1>
-          <p><b>Genre:</b> ${storage[i].Genre}</p>
-          <p><b>Actors:</b> ${storage[i].Actors}</p>
-          <p><b>Year:</b> ${storage[i].Year}</p>
-          <p><b>Rated:</b> ${storage[i].Rated}</p>
-          <p><b>Plot:</b> ${storage[i].Plot}</p>
-          <p><b>Director:</b> ${storage[i].Director}</p>
-          <p><b>Runtime:</b> ${storage[i].Runtime}</p>
-        </div>
+   
     </div>
  `);
-  acc = document.getElementsByClassName("accordion");
-  createBtn();
- console.log(acc);
   }
 }
 
@@ -77,18 +71,14 @@ $("#search-Btn").on("click", function (event) {
   year = $("#year").val().trim().toLowerCase();
 
   getData(title, year);
-  document.querySelector("#movieCard-container").scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-    inline: "start",
-  });
+  // document.querySelector("#movieCard-container").scrollIntoView({
+  //   behavior: "smooth",
+  //   block: "start",
+  //   inline: "start",
+  // });
   document.getElementById("myForm").reset();
-  $("#movieCard").remove();
+  // $("#movieCard").remove();
 });
-
-if (localStorage.getItem("saved-movies-series") === null) {
-  localStorage.setItem("saved-movies-series", JSON.stringify([]));
-}
 
 $("#empty-storage-btn").on("click", (event) => {
   event.preventDefault();
@@ -97,27 +87,17 @@ $("#empty-storage-btn").on("click", (event) => {
   location.reload();
 });
 
-// let acc = document.getElementsByClassName("accordion");
+myf = (event) => {
+  event.preventDefault();
+  let el = event.target;
+  el = el.id;
 
-// console.log(acc);
-function createBtn () {
-if (acc.length > 1 ){
-  console.log(acc);
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    console.log(panel);
-    if (panel.style.height === 0) {
-      panel.style.height = 100 + "%";
-    } else {
-      panel.style.height = 0;
-    }
-  });
-}
-} else {
-  acc = [];
+  // console.log(el)
+  getData(el);
+};
+
+if (localStorage.getItem("saved-movies-series") === null) {
+  localStorage.setItem("saved-movies-series", JSON.stringify([]));
 }
 
-}
 renderStorage();
